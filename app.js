786cc04b0118149ed28220307d167c94d406bd0e
Judy <158211720+judyyan27@users.noreply.github.com>
2024-11-13 12:20:03 -0500

    e

---
+++ app.js
@@ -0,0 +1,22 @@
+// gets data
+// shows the data (usually happens while getting data)
+//
+
+async function getData() {
+  // fetch returns a promise -- promises that someimte in the future it returns something
+  try {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon/ditto");
+    // guard clause
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      const data = await response.json(); // json"ified" with data we can use
+      console.log(data);
+      document.querySelector("h1").textContent = data.name;
+    }
+  } catch (error) {
+    console.log(error);
+    console.log("sorry could not find that pocket monster");
+  }
+}
+getData();
