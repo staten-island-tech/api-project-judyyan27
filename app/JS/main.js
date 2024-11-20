b2f0cf1878417d11abbf1a7cd0250625dadb80a4
Judy <158211720+judyyan27@users.noreply.github.com>
2024-11-20 12:30:17 -0500

    working data

--- app/JS/main.js
+++ app/JS/main.js
@@ -1,21 +1,28 @@
+import "../CSS/style.css";
+
+const DOMSelectors = {
+  button: document.querySelector(`.button`),
+  container: document.querySelector(`.container-box`),
+  reset: document.querySelector(`#reset`),
+};
+
 // gets data
 // shows the data (usually happens while getting data)
-//
 
 async function testData() {
   // fetch returns a promise -- promises that someimte in the future it returns something
   try {
     const response = await fetch(
-      "https://potterapi-fedeperin.vercel.app/es/characters?search=Ron%20Weasley"
+      "https://potterapi-fedeperin.vercel.app/es/characters"
     );
     // guard clause
     if (response.status != 200) {
       throw new Error(response);
     } else {
-      const data = await response.json(); // json"ified" with data we can use
-      console.log(data);
-      document.querySelector("h1").textContent = data[0].fullName;
-      //
+      const hpData = await response.json(); // json"ified" with data we can use
+      console.log(hpData);
+      document.querySelector("h1").textContent = hpData[0].fullName;
+      createCards(hpData);
     }
   } catch (error) {
     console.log(error);
@@ -23,5 +30,26 @@ async function testData() {
   }
 }
 testData();
+//hpData is the name for the jsonified data
 
-function getData() {}
+//standard function that creates cards for flowers on screen and can accept different arrays
+function createCards(hpData) {
+  DOMSelectors.container.innerHTML = "";
+  hpData.forEach((character) =>
+    DOMSelectors.container.insertAdjacentHTML(
+      "beforeend",
+      `<div class="card">
+        <img class="card-img" src="${character.image}" alt="Image of ${
+        character.fullName
+      }" />
+        <h2 class="card-title">Name: ${character.fullName}</h2>
+        <h3 class="card-price">Birthdate: ${character.birthdate}</h3>
+        <h3 class="card-desc">House: ${character.hogwartsHouse}</h3>
+        <h3 class="card-desc">Actor: ${character.interpretedBy || "N/A"}</h3>
+      </div>`
+    )
+  );
+}
+
+// Upon loading website, all cards show on screen
+createCards(hpData);
