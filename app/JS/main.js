cdde36321cb405cb3a3d2392021b7f77893c4d8f
Judy <158211720+judyyan27@users.noreply.github.com>
2024-11-27 11:48:43 -0500

    spells and books

--- app/JS/main.js
+++ app/JS/main.js
@@ -10,6 +10,7 @@ const DOMSelectors = {
   hufflepuff: document.querySelector(`#hufflepuff`),
   ravenclaw: document.querySelector(`#ravenclaw`),
 
+  characters: document.querySelector(`#characters`),
   books: document.querySelector(`#books`),
   spells: document.querySelector(`#spells`),
 };
@@ -17,7 +18,7 @@ const DOMSelectors = {
 async function normalData() {
   // fetch returns a promise -- promises that someimte in the future it returns something
   try {
-    const response = await fetch(
+    let response = await fetch(
       "https://potterapi-fedeperin.vercel.app/es/characters"
     );
     // guard clause
@@ -40,18 +41,19 @@ normalData();
 async function griffindorData() {
   // fetch returns a promise -- promises that someimte in the future it returns something
   try {
-    const response = await fetch(
+    let response = await fetch(
       "https://potterapi-fedeperin.vercel.app/es/characters"
     );
     // guard clause
     if (response.status != 200) {
       throw new Error(response);
     } else {
-      const griffindor = await response.json(); // json"ified" with data we can use
+      let griffindor = await response.json(); // json"ified" with data we can use
       //if the character's house is griffindor-- save that info as another data
-      const data = griffindor.filter((house) =>
-        house.hogwartsHouse.includes("griffindor")
+      let gdata = griffindor.filter(
+        (house) => house.hogwartsHouse === "Griffindor"
       );
+      createCards(gdata);
     }
   } catch (error) {
     console.log(error);
@@ -82,3 +84,81 @@ function createCards(hpData) {
 }
 // gets data
 // shows the data (usually happens while getting data)
+
+function createBooks(hpData) {
+  DOMSelectors.container.innerHTML = "";
+  hpData.forEach((book) =>
+    DOMSelectors.container.insertAdjacentHTML(
+      "beforeend",
+      `<div class="card bg-rose-50 w-80 h-80">
+        <img class="card-img" src="${book.cover}" alt="Image of ${book.originalTitle} Movie Poster" />
+        <h2 class="card-title bg-rgb(184, 134, 11)">${book.title}</h2>
+        <h3 class="card-price">Release Date: ${book.releaseDate}</h3>
+        <h3 class="card-desc">Description: ${book.description}</h3>
+        <h3 class="card-desc">Number of Pages: ${book.pages}</h3>
+      </div>`
+    )
+  );
+}
+
+async function bookData() {
+  // fetch returns a promise -- promises that someimte in the future it returns something
+  try {
+    let response = await fetch(
+      "https://potterapi-fedeperin.vercel.app/en/books"
+    );
+    // guard clause
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      const books = await response.json(); // json"ified" with data we can use
+      console.log(books);
+      createBooks(books);
+    }
+  } catch (error) {
+    console.log(error);
+    console.log("sorry could not find that information");
+  }
+}
+
+DOMSelectors.books.addEventListener("click", function () {
+  DOMSelectors.container.innerHTML = "";
+  bookData();
+});
+
+function createSpells(hpData) {
+  DOMSelectors.container.innerHTML = "";
+  hpData.forEach((spell) =>
+    DOMSelectors.container.insertAdjacentHTML(
+      "beforeend",
+      `<div class="card bg-rose-50 w-80 h-80">
+        <h2 class="card-title bg-rgb(184, 134, 11)">Spell ame: ${spell.spell}</h2>
+        <h3 class="card-price">Use: ${spell.use}</h3>`
+    )
+  );
+}
+
+async function spellsData() {
+  // fetch returns a promise -- promises that someimte in the future it returns something
+  try {
+    let response = await fetch(
+      "https://potterapi-fedeperin.vercel.app/en/spells"
+    );
+    // guard clause
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      const spells = await response.json(); // json"ified" with data we can use
+      console.log(spells);
+      createSpells(spells);
+    }
+  } catch (error) {
+    console.log(error);
+    console.log("sorry could not find that information");
+  }
+}
+
+DOMSelectors.spells.addEventListener("click", function () {
+  DOMSelectors.container.innerHTML = "";
+  spellsData();
+});
